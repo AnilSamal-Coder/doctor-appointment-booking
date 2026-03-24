9ceac4ed31bc49939501f6bf51b553bc69fbe910
AnilSamal-Coder <anilsamal015@gmail.com>
2026-03-24 12:04:19 +0530

    Feature branch 2 (#3)
    * remove testing
    
    * updating the prfile and appointment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import logo from '../assets/medslotifynow.png'
 import { NavLink, useNavigate } from 'react-router-dom'
 import profile_pic from '../assets/profile_pic.png'
+import { assets } from '../assets/assets'
 
 const Navbar = () => {
 
@@ -75,6 +76,20 @@ const [token, setToken] = useState(true)
     Create Account
   </button>
 )}
+ <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+ {/* ----Mobile Menu---- */}
+ <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0' } md:hidden right-0 top-0 bottom-0 z-0 overflow-hidden bg-white transition-all`}>
+  <div className='flex items-center justify-between px-5 py-6'>
+    <img className='w-36' src={assets.medslotifynow} alt="" />
+    <img className='w-7' onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
+  </div>
+  <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+    <NavLink  onClick={()=>setShowMenu(false)} to='/'><p className='px-4 py-2 rounded inline-block'>HOME</p></NavLink>
+    <NavLink  onClick={()=>setShowMenu(false)} to='/doctors'><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
+    <NavLink  onClick={()=>setShowMenu(false)} to='/about'><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
+    <NavLink  onClick={()=>setShowMenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
+  </ul>
+ </div>
       </div>
     </div>
   )
